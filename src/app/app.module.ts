3893e9ed6e640ef7568cd2ffad1945f5bd213d5e
Varuni-Punchihewa <1995varuni@gmail.com>
2018-05-13 00:01:40 +0530

    git branch tourPlanner1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,21 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+
+import { GooglePlus } from '@ionic-native/google-plus';
+import { AngularFireModule } from 'angularfire2';
+import firebase from 'firebase';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCqri1Mr0dwhe9HeBX1q-Vrz-H3kdcBX_g",
+    authDomain: "group-project1-1490666790343.firebaseapp.com",
+    databaseURL: "https://group-project1-1490666790343.firebaseio.com",
+    projectId: "group-project1-1490666790343",
+    storageBucket: "",
+    messagingSenderId: "117493625647"
+}
+
+firebase.initializeApp(firebaseConfig);
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +29,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -22,6 +38,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+   GooglePlus,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { AngularFireModule } from 'angularfire2';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public googleplus:GooglePlus) {
 
+  }
+  login(){
+    this.googleplus.login({
+      'webClientId':'117493625647-6dvi9s42um3ldjeoje4qkfvuhnluc6vc.apps.googleusercontent.com',
+      'offline':true
+    }).then(res=>{
+      firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken)).then(suc=>{
+        alert("LOGIN SUCCESSFULLY")
+      }).catch(ns=>{
+        alert("NOT SUCCESS")
+      })
+    })
   }
 
 }
